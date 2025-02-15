86fc0715e74d18bf58161fa2c60b6696ecb185c0
dsmd <lex.mironchenko@gmail.com>
2025-02-15 03:09:59 +0000

    liquibase/testcontainers fix

--- run_tests.ps1
+++ run_tests.ps1
@@ -1,9 +1,9 @@
-# Run unit tests
+# Run unit tests with output visible
 Write-Host "Running unit tests..."
-pytest tests/unit --maxfail=0 --disable-warnings --html=unit_report.html
+pytest tests/unit -s --maxfail=0 --disable-warnings --html=unit_report.html
 if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 
-# Run integration tests
+# Run integration tests with output visible
 Write-Host "Running integration tests..."
-pytest tests/integration --maxfail=0 --disable-warnings --html=integration_report.html
+pytest tests/integration -s --maxfail=0 --disable-warnings --html=integration_report.html
 exit $LASTEXITCODE
